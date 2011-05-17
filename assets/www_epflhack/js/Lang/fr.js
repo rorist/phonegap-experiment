efebbe1bf76dfe03d3fe038f8c951b40298ccdd3
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2011-05-17 12:07:33 +0200

    Epfl from webspidering

---
+++ assets/www_epflhack/js/Lang/fr.js
@@ -0,0 +1,13 @@
+/**
+ * @requires OpenLayers/Lang.js
+ */
+OpenLayers.Lang.fr = OpenLayers.Util.applyDefaults({
+    "error_get_position" : "Impossible d'obtenir la position actuelle",
+    "no_results"         : "Pas de résultats",
+    'category'           : 'Catégorie',
+    'layer'              : 'Couche',
+    'room'               : 'Salle',
+    'ortho'              : 'Satellite',
+    'plan'               : 'Plans'
+});
+
