6f5e14053e190dbaadbd3f62a9d287b94ada826e
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2011-05-26 17:31:39 +0200

    try to use a phionegap plugin for canvas.toDataURL()

--- assets/www/m/js/OSM.js
+++ assets/www/m/js/OSM.js
@@ -20,13 +20,14 @@ app.OSM = OpenLayers.Class(OpenLayers.Layer.OSM, {
     },
 
     getURL: function (bounds, imgDiv) {
+        var ctxt = this;
         if (this.map.getZoom() > 18) {
             return app.blankImageURL;
         } else {
             if(window.requestFileSystem){
                 var url = OpenLayers.Layer.OSM.prototype.getURL.apply(this, arguments);
                 window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs){
-                    fs.root.getFile("Android/com.camptocamp.phonegap/" + urlFriendly(url), {}, function(entry){
+                    fs.root.getFile("Android/com.camptocamp.phonegap/tiles/" + urlFriendly(url), {}, function(entry){
                         entry.file(function(){
                             var reader = new FileReader();
                             reader.onloadend = function(e){
@@ -34,19 +35,54 @@ app.OSM = OpenLayers.Class(OpenLayers.Layer.OSM, {
                             }
                             reader.readAsDataURL(file);
                         }, function(){
+                            imgDiv.onload = function(){ctxt.saveImage(imgDiv, url);};
                             imgDiv.src = url;
                         });
                     }, function(){
+                        imgDiv.onload = function(){ctxt.saveImage(imgDiv, url);};
                         imgDiv.src = url;
                     });
                 }, function(){
                     if(imgDiv != null){
+                        imgDiv.onload = OpenLayers.Function.bindAsEventListener(ctxt.saveImage, imgDiv);
                         imgDiv.src = url;
                     }
                 });
             }
             return app.blankImageURL;
         }
+    },
+    
+    saveImage: function(imgDiv){
+    
+        var error = function(e){
+            console.log("error "+e.code);
+        }
+        
+        var url = imgDiv.src;
+        var cnv = document.createElement("canvas");
+        cnv.width = imgDiv.offsetWidth;
+        cnv.height = imgDiv.offsetHeight;
+        var ctx = cnv.getContext("2d");
+        ctx.drawImage(imgDiv, 0, 0);
+        
+        window.plugins.canvas.toDataURL(cnv, "image/png", function(arg){
+            var data = arg.data.replace(/^data:image\/png;base64,/, "");
+            console.log(data);
+            if(data=="data:,"){
+                return;
+            }
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs){
+                fs.root.getFile("Android/com.camptocamp.phonegap/tiles/" + urlFriendly(url), {create: true}, function(entry){
+                    entry.createWriter(function(writer){
+                        writer.onwrite = function(e){
+                            console.log("written");
+                        };
+                        writer.write(data);
+                    }, error);
+                }, error);
+            }, error);
+        }, error);
     },
 
     CLASS_NAME: "app.OSM"
--- src/com/camptocamp/gis/phonegap/App.java
+++ src/com/camptocamp/gis/phonegap/App.java
@@ -1,8 +1,17 @@
 package com.camptocamp.gis.phonegap;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebSettings.LayoutAlgorithm;
+import android.widget.LinearLayout;
 
+import com.phonegap.CallbackServer;
 import com.phonegap.DroidGap;
+import com.phonegap.WebViewReflect;
+import com.phonegap.api.PluginManager;
 
 public class App extends DroidGap {
 
@@ -18,116 +27,120 @@ public class App extends DroidGap {
         loadUrl("file:///android_asset/www/index.html");
     }
 
-    // @Override
-    // public void init() {
-    //
-    // // Create web container
-    // this.appView = new WebView(App.this) {
-    // @Override
-    // public boolean onTouchEvent(MotionEvent ev) {
-    //
-    // if (ev.getAction() == MotionEvent.ACTION_POINTER_2_DOWN) {
-    // // Gesture start
-    // sendJavascript("alert('salut');");
-    //                    
-    // // String start = String.format(EVT_START, ev.getX(0), ev.getY(0), ev.getX(1),
-    // // ev
-    // // .getY(1));
-    // // appView.loadUrl("javascript:pinch.touchstart(" + start + ")");
-    // // Log.e("TEST", "start="+start);
-    // return true;
-    // }
-    // if (ev.getAction() == MotionEvent.ACTION_POINTER_1_UP
-    // || ev.getAction() == MotionEvent.ACTION_POINTER_2_UP) {
-    // // Gesture End
-    //                    
-    // // appView.loadUrl("javascript:pinch.touchend(" + EVT_END + ")");
-    // return true;
-    // }
-    //
-    // if (ev.getPointerCount() > 1) {
-    // // Gesture Move
-    //                    
-    // // String move = String.format(EVT_MOVE, ev.getX(0), ev.getY(0), ev.getX(1), ev
-    // // .getY(1));
-    // // appView.loadUrl("javascript:pinch.touchmove(" + move + ")");
-    // // Log.e("TEST", "move="+move);
-    // }
-    // return super.onTouchEvent(ev);
-    // }
-    // };
-    // this.appView.setId(100);
-    //
-    // this.appView.setLayoutParams(new LinearLayout.LayoutParams(
-    // ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT, 1.0F));
-    //
-    // WebViewReflect.checkCompatibility();
-    //
-    // if (android.os.Build.VERSION.RELEASE.startsWith("1.")) {
-    // this.appView.setWebChromeClient(new GapClient(App.this));
-    // }
-    // else {
-    // this.appView.setWebChromeClient(new EclairClient(App.this));
-    // }
-    //
-    // this.setWebViewClient(this.appView, new GapViewClient(this));
-    //
-    // this.appView.setInitialScale(100);
-    // this.appView.setVerticalScrollBarEnabled(false);
-    // this.appView.requestFocusFromTouch();
-    //
-    // // Enable JavaScript
-    // WebSettings settings = this.appView.getSettings();
-    // settings.setJavaScriptEnabled(true);
-    // settings.setJavaScriptCanOpenWindowsAutomatically(true);
-    // settings.setLayoutAlgorithm(LayoutAlgorithm.NORMAL);
-    //
-    // // Enable database
-    // Package pack = this.getClass().getPackage();
-    // String appPackage = pack.getName();
-    // WebViewReflect.setStorage(settings, true, "/data/data/" + appPackage + "/app_database/");
-    //
-    // // Enable DOM storage
-    // WebViewReflect.setDomStorage(settings);
-    //
-    // // Enable built-in geolocation
-    // WebViewReflect.setGeolocationEnabled(settings, true);
-    //
-    // // Bind PhoneGap objects to JavaScript
-    // // this.bindBrowser(this.appView);
-    // this.callbackServer = new CallbackServer();
-    // this.pluginManager = new PluginManager(appView, this);
-    // this.addService("App", "com.phonegap.App");
-    // this.addService("Geolocation", "com.phonegap.GeoBroker");
-    // this.addService("Device", "com.phonegap.Device");
-    // this.addService("Accelerometer", "com.phonegap.AccelListener");
-    // this.addService("Compass", "com.phonegap.CompassListener");
-    // this.addService("Media", "com.phonegap.AudioHandler");
-    // this.addService("Camera", "com.phonegap.CameraLauncher");
-    // this.addService("Contacts", "com.phonegap.ContactManager");
-    // this.addService("Crypto", "com.phonegap.CryptoHandler");
-    // this.addService("File", "com.phonegap.FileUtils");
-    // this.addService("Location", "com.phonegap.GeoBroker");
-    // this.addService("Network Status", "com.phonegap.NetworkManager");
-    // this.addService("Notification", "com.phonegap.Notification");
-    // this.addService("Storage", "com.phonegap.Storage");
-    // this.addService("Temperature", "com.phonegap.TempListener");
-    // this.addService("FileTransfer", "com.phonegap.FileTransfer");
-    // this.addService("Capture", "com.phonegap.Capture");
-    //
-    // // Add web view but make it invisible while loading URL
-    // this.appView.setVisibility(View.INVISIBLE);
-    // root.addView(this.appView);
-    // setContentView(root);
-    //
-    // // Clear cancel flag
-    // this.cancelLoadUrl = false;
-    //
-    // // If url specified, then load it
-    // String url = this.getStringProperty("url", null);
-    // if (url != null) {
-    // System.out.println("Loading initial URL=" + url);
-    // this.loadUrl(url);
-    // }
-    // }
+    @Override
+    public void init() {
+
+        // Create web container
+        this.appView = new WebView(App.this);
+        // this.appView = new WebView(App.this) {
+        // @Override
+        // public boolean onTouchEvent(MotionEvent ev) {
+        //
+        // if (ev.getAction() == MotionEvent.ACTION_POINTER_2_DOWN) {
+        // // Gesture start
+        // sendJavascript("alert('salut');");
+        //
+        // // String start = String.format(EVT_START, ev.getX(0), ev.getY(0), ev.getX(1),
+        // // ev
+        // // .getY(1));
+        // // appView.loadUrl("javascript:pinch.touchstart(" + start + ")");
+        // // Log.e("TEST", "start="+start);
+        // return true;
+        // }
+        // if (ev.getAction() == MotionEvent.ACTION_POINTER_1_UP
+        // || ev.getAction() == MotionEvent.ACTION_POINTER_2_UP) {
+        // // Gesture End
+        //
+        // // appView.loadUrl("javascript:pinch.touchend(" + EVT_END + ")");
+        // return true;
+        // }
+        //
+        // if (ev.getPointerCount() > 1) {
+        // // Gesture Move
+        //
+        // // String move = String.format(EVT_MOVE, ev.getX(0), ev.getY(0), ev.getX(1), ev
+        // // .getY(1));
+        // // appView.loadUrl("javascript:pinch.touchmove(" + move + ")");
+        // // Log.e("TEST", "move="+move);
+        // }
+        // return super.onTouchEvent(ev);
+        // }
+        // };
+        this.appView.setId(100);
+
+        this.appView.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT, 1.0F));
+
+        WebViewReflect.checkCompatibility();
+
+        if (android.os.Build.VERSION.RELEASE.startsWith("1.")) {
+            this.appView.setWebChromeClient(new GapClient(App.this));
+        }
+        else {
+            this.appView.setWebChromeClient(new EclairClient(App.this));
+        }
+
+        this.setWebViewClient(this.appView, new GapViewClient(this));
+
+        this.appView.setInitialScale(100);
+        this.appView.setVerticalScrollBarEnabled(false);
+        this.appView.requestFocusFromTouch();
+
+        // Enable JavaScript
+        WebSettings settings = this.appView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setLayoutAlgorithm(LayoutAlgorithm.NORMAL);
+        
+        // FORCE NOCACHE
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+        // Enable database
+        Package pack = this.getClass().getPackage();
+        String appPackage = pack.getName();
+        WebViewReflect.setStorage(settings, true, "/data/data/" + appPackage + "/app_database/");
+
+        // Enable DOM storage
+        WebViewReflect.setDomStorage(settings);
+
+        // Enable built-in geolocation
+        WebViewReflect.setGeolocationEnabled(settings, true);
+
+        // Bind PhoneGap objects to JavaScript
+        // this.bindBrowser(this.appView);
+        this.callbackServer = new CallbackServer();
+        this.pluginManager = new PluginManager(appView, this);
+        this.addService("App", "com.phonegap.App");
+        this.addService("Geolocation", "com.phonegap.GeoBroker");
+        this.addService("Device", "com.phonegap.Device");
+        this.addService("Accelerometer", "com.phonegap.AccelListener");
+        this.addService("Compass", "com.phonegap.CompassListener");
+        this.addService("Media", "com.phonegap.AudioHandler");
+        this.addService("Camera", "com.phonegap.CameraLauncher");
+        this.addService("Contacts", "com.phonegap.ContactManager");
+        this.addService("Crypto", "com.phonegap.CryptoHandler");
+        this.addService("File", "com.phonegap.FileUtils");
+        this.addService("Location", "com.phonegap.GeoBroker");
+        this.addService("Network Status", "com.phonegap.NetworkManager");
+        this.addService("Notification", "com.phonegap.Notification");
+        this.addService("Storage", "com.phonegap.Storage");
+        this.addService("Temperature", "com.phonegap.TempListener");
+        this.addService("FileTransfer", "com.phonegap.FileTransfer");
+        this.addService("Capture", "com.phonegap.Capture");
+
+        // Add web view but make it invisible while loading URL
+        this.appView.setVisibility(View.INVISIBLE);
+        root.addView(this.appView);
+        setContentView(root);
+
+        // Clear cancel flag
+        this.cancelLoadUrl = false;
+
+        // If url specified, then load it
+        String url = this.getStringProperty("url", null);
+        if (url != null) {
+            System.out.println("Loading initial URL=" + url);
+            this.loadUrl(url);
+        }
+    }
 }
