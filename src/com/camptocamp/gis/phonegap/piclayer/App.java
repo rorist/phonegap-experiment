194eb7ab85707d18c68ea6b3118950137394ce42
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2011-06-24 11:57:05 +0200

    Some tweaks: splash screen, clean cache for the whole app, do not run in the bg, greater timeout at startup

--- src/com/camptocamp/gis/phonegap/piclayer/App.java
+++ src/com/camptocamp/gis/phonegap/piclayer/App.java
@@ -8,7 +8,11 @@ public class App extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        clearCache();
-        loadUrl("file:///android_asset/www/index.html");
+        super.init();
+        super.appView.clearCache(true);
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
+        super.setIntegerProperty("loadUrlTimeoutValue", 60000);
+        super.setBooleanProperty("keepRunning", false);
+        loadUrl("file:///android_asset/www/index.html", 1000);
     }
 }
